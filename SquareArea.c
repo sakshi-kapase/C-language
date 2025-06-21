24b4d94c25ddf9ac91802dd1405e23a4336a55c5
sakshi-kapase <sakshikapase2005@gmail.com>
2025-06-21 15:23:07 +0530

    Add files via upload

---
+++ SquareArea.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int side,area;
+    printf("Enter side =");
+    scanf("%d",&side);
+
+    area=side*side;
+    printf("Area of square = %d",area);
+    return 0;
+}
